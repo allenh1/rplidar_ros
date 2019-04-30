052dc84e4603f3f1355575bacc541980d07f3cb6
Hunter L. Allen <hallen@kns.com>
2019-04-29 21:14:45 -0400

    Begin implementation of composable rplidar_ros::rplidar_node
    Signed-off-by: Hunter L. Allen <hallen@kns.com>

---
+++ include/rplidar_node.hpp
@@ -0,0 +1,95 @@
+/*
+ *  RPLIDAR ROS NODE
+ *
+ *  Copyright (c) 2009 - 2014 RoboPeak Team
+ *  http://www.robopeak.com
+ *  Copyright (c) 2014 - 2016 Shanghai Slamtec Co., Ltd.
+ *  http://www.slamtec.com
+ *
+ */
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+#ifndef RPLIDAR_NODE_HPP_
+#define RPLIDAR_NODE_HPP_
+
+#include <rclcpp/clock.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/time.hpp>
+#include <rclcpp/time_source.hpp>
+
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <std_srvs/srv/empty.hpp>
+
+#include <rplidar.h>
+
+#ifndef _countof
+#define _countof(_Array) (int)(sizeof(_Array) / sizeof(_Array[0]))
+#endif
+
+#define M_PI 3.1415926535897932384626433832795
+#define DEG2RAD(x) ((x)*M_PI/180.)
+
+namespace
+{
+using LaserScan = sensor_msgs::msg::LaserScan;
+using LaserScanPub = rclcpp::Publisher<LaserScan>::SharedPtr;
+using RPlidarDriver =  rp::standalone::rplidar::RPlidarDriver;
+}
+
+namespace rplidar_ros
+{
+
+class rplidar_node : public rclcpp::Node
+{
+public:
+  explicit rplidar_node(rclcpp::Node::options options);
+  virtual ~rplidar_node();
+
+  void publish_scan();
+
+private:
+  /* parameters */
+  std::string channel_type_;
+  std::string tcp_ip_;
+  std::string serial_port_;
+  int tcp_port_;
+  int serial_baudrate_;
+  std::string frame_id_;
+  bool inverted_;
+  bool angle_compensate;
+  bool angle_compensate_;
+  std::string scan_mode_;
+  /* Publisher */
+  LaserScanPub m_publisher;
+  /* SDK Pointer */
+  RPlidarDriver * m_drv = nullptr;
+  size_t m_scan_count = 0;
+  const float max_distance = 8.0f;
+  const fload min_distance = 0.15f;
+};
+
+}  // namespace rplidar_ros
+
+#endif  // RPLIDAR_NODE_HPP_
---
+++ src/rplidar_node.cpp
@@ -0,0 +1,124 @@
+/*
+ *  RPLIDAR ROS NODE
+ *
+ *  Copyright (c) 2009 - 2014 RoboPeak Team
+ *  http://www.robopeak.com
+ *  Copyright (c) 2014 - 2016 Shanghai Slamtec Co., Ltd.
+ *  http://www.slamtec.com
+ *
+ */
+/*
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <rplidar_node.hpp>
+
+namespace rplidar_ros
+{
+
+rplidar_node::rplidar_node(rclcpp::Node::options options)
+  : rclcpp::Node(options, "rplidar_node")
+{
+  /* set parameters */
+  node->get_parameter_or("channel_type", channel_type_, std::string("serial"));
+  node->get_parameter_or("tcp_ip", tcp_ip_, std::string("192.168.0.7"));
+  node->get_parameter_or("tcp_port", tcp_port_, 20108);
+  node->get_parameter_or("serial_port", serial_port_, std::string("/dev/ttyUSB0"));
+  node->get_parameter_or("serial_baudrate", serial_baudrate_, 115200);
+  node->get_parameter_or("frame_id", frame_id_, std::string("laser_frame"));
+  node->get_parameter_or("inverted", inverted_, false);
+  node->get_parameter_or("angle_compensate", angle_compensate_, false);
+  node->get_parameter_or("scan_mode", scan_mode_, std::string());
+
+  /* set QoS settings */
+  rmw_qos_profile_t qos = rmw_qos_profile_default;
+  qos.depth = 1;
+  qos.durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL;
+
+  /* create the publisher for "/scan" */
+  m_publisher = this->create_publisher<LaserScan>("scan", qos);
+
+  RCLCPP_INFO(this->get_logger(),
+    "RPLIDAR running on ROS 2 package rplidar_ros. SDK Version: '%s'", RPLIDAR_SDK_VERSION);
+
+  /* initialize SDK */
+  m_drv = (channel_type == "tcp")
+    ? RPlidarDriver::CreateDriver(rp::standalone::rplidar::DRIVER_TYPE_TCP)
+    : RPlidarDriver::CreateDriver(rp::standalone::rplidar::DRIVER_TYPE_SERIALPORT);
+
+}
+
+void rplidar_node::publish_scan()
+{
+  static size_t scan_count = 0;
+  sensor_msgs::msg::LaserScan scan_msg;
+
+  /* NOTE(allenh1): time was passed in as a parameter before */
+  scan_msg.header.stamp = clock->now();
+  scan_msg.header.frame_id = frame_id_;
+  scan_count++;
+
+  bool reversed = (angle_max > angle_min);
+  if ( reversed ) {
+    scan_msg.angle_min =  M_PI - angle_max;
+    scan_msg.angle_max =  M_PI - angle_min;
+  } else {
+    scan_msg.angle_min =  M_PI - angle_min;
+    scan_msg.angle_max =  M_PI - angle_max;
+  }
+  scan_msg.angle_increment =
+    (scan_msg.angle_max - scan_msg.angle_min) / (double)(node_count-1);
+
+  scan_msg.scan_time = scan_time;
+  scan_msg.time_increment = scan_time / (double)(node_count-1);
+  scan_msg.range_min = min_distance;
+  scan_msg.range_max = max_distance;
+
+  scan_msg.intensities.resize(node_count);
+  scan_msg.ranges.resize(node_count);
+  bool reverse_data = (!inverted_ && reversed) || (inverted_ && !reversed);
+  if (!reverse_data) {
+    for (size_t i = 0; i < node_count; i++) {
+      float read_value = (float) nodes[i].dist_mm_q2/4.0f/1000;
+      if (read_value == 0.0)
+        scan_msg.ranges[i] = std::numeric_limits<float>::infinity();
+      else
+        scan_msg.ranges[i] = read_value;
+      scan_msg.intensities[i] = (float) (nodes[i].quality >> 2);
+    }
+  } else {
+    for (size_t i = 0; i < node_count; i++) {
+      float read_value = (float)nodes[i].dist_mm_q2/4.0f/1000;
+      if (read_value == 0.0)
+        scan_msg.ranges[node_count-1-i] = std::numeric_limits<float>::infinity();
+      else
+        scan_msg.ranges[node_count-1-i] = read_value;
+      scan_msg.intensities[node_count-1-i] = (float) (nodes[i].quality >> 2);
+    }
+  }
+
+  pub->publish(scan_msg);
+}
+
+}  // namespace rplidar_ros
